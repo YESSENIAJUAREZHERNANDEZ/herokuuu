d9666d20bfadff7208bb41d5250b44c74b873ae6
YESSENIAJUAREZHERNANDEZ <1720110342@utectulancingo.edu.mx>
2022-03-07 22:24:10 +0000

    LOOOOO

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class hello:
     def GET(self, name):
         if not name:
             name = 'World'
-        return 'Hello, ' + name + '!'
+        return 'Hellow, ' + name + '!'
 
 if __name__ == "__main__":
     app.run()
